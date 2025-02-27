fd5066389d7782cd93153a99b85f158e8395c507
Arnab <arnabsantra248@gmail.com>
2025-02-28 00:03:20 +0530

    add display code page

--- src/app/[filename]/page.tsx
+++ src/app/[filename]/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { ApiResponse } from "@/components/cform";
 import {
 	Card,
@@ -7,50 +8,88 @@ import {
 	CardTitle,
 } from "@/components/ui/card";
 import axios, { AxiosError } from "axios";
+import { useParams } from "next/navigation";
+import { useCallback, useEffect, useState } from "react";
+// import dynamic from "next/dynamic";
+import { dracula, CopyBlock } from "react-code-blocks";
 
-const CodePage = async ({
-	params,
-}: {
-	params: Promise<{ filename: string }>;
-}) => {
-	const { filename } = await params;
+interface fileDetailsProps {
+	filename: string;
+	filetype?: string;
+	content: string;
+	description?: string;
+}
+
+const CodePage = () => {
+	const { filename } = useParams<{ filename: string }>();
+	// console.log(params);
+	// const [isCopied, setIsCopied] = useState(false);
+	const [fileDetails, setFileDetails] = useState<fileDetailsProps>();
 	const base_url = process.env.NEXT_PUBLIC_BASE_URL || "";
-	try {
-		const res = await axios.get<ApiResponse>(
-			`${base_url}/api/getData?filename=${filename}`
-		);
-		// console.log(res);
-		const file = res.data.code;
-		if (!file) {
-			return <div>Bye</div>;
-		}
 
+	const fetchFileDetails = useCallback(async () => {
+		try {
+			const res = await axios.get<ApiResponse>(
+				`${base_url}/api/getData?filename=${filename}`
+			);
+			// console.log(res);
+			const file = res.data.code;
+			if (!file) {
+				return <div>Bye</div>;
+			}
+
+			setFileDetails({
+				filename: file.fileName,
+				content: file.content,
+				filetype: file.fileType,
+				description: file.description,
+			});
+		} catch (error) {
+			const axiosError = error as AxiosError<ApiResponse>;
+			console.log("Error happened", error);
+
+			return <div>{axiosError.response?.data.message}</div>;
+		}
+	}, [base_url, filename]);
+
+	useEffect(() => {
+		fetchFileDetails();
+	}, [fetchFileDetails, filename]);
+
+	if (fileDetails) {
 		return (
 			<div className="flex flex-col gap-8">
 				{/* <div>
 					<Navbar link="/addForm" displayText="Add"></Navbar>
 				</div> */}
 				<div className="flex items-center justify-center p-4">
-					<Card className="w-fit">
+					<Card className="min-w-96 min-h-36">
 						<CardHeader>
-							<CardTitle>{file.fileName}</CardTitle>
+							<CardTitle>{fileDetails.filename}</CardTitle>
 							<CardDescription>
-								{file.description}
+								{fileDetails.description}
 							</CardDescription>
 						</CardHeader>
-						<CardContent>{file.content}</CardContent>
+						<CardContent>
+							<CopyBlock
+								text={fileDetails.content}
+								language={fileDetails.filetype || "javascript"}
+								showLineNumbers={true}
+								theme={dracula}
+								// copied={isCopied}
+								// onCopy={() => {
+								// 	setIsCopied(!isCopied);
+								// }}
+								wrapLongLines
+							/>
+						</CardContent>
 					</Card>
 				</div>
 			</div>
 		);
-	} catch (error) {
-		const axiosError = error as AxiosError<ApiResponse>;
-		console.log("Error happened", error);
-
-		return <div>{axiosError.response?.data.message}</div>;
 	}
 
-	// console.log(file);
+	return <div>Loading...</div>;
 };
 
 export default CodePage;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Toaster } from "@/components/ui/sonner";
 import Navbar from "@/components/navbar";
 const geistSans = Geist({
@@ -32,6 +33,7 @@ export default function RootLayout({
 				<Navbar />
 				{children}
 				<Analytics />
+				<SpeedInsights />
 				<Toaster />
 			</body>
 		</html>
